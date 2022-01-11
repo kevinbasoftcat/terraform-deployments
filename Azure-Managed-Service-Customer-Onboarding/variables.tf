727c8e49168a452d456c8c52e8c967b300066cd8
kevinbasoftcat <kevinba@softcat.com>
2022-01-11 10:38:56 +0000

    Updated variable formatting

--- Azure-Managed-Service-Customer-Onboarding/variables.tf
+++ Azure-Managed-Service-Customer-Onboarding/variables.tf
@@ -1,5 +1,4 @@
 variable "customer_tenant_id" {
     type = string
-    description = "Customer Azure AD Tenant ID"
-    default = "${{ github.event.inputs.CustomerTenantID }}"
+    description = "Customer Azure AD Tenant ID" 
 }
